029783305ef4da1562eabd0019f7a249b6819b19
statinbower3 <135834283+statinbower3@users.noreply.github.com>
2026-05-27 05:36:54 +1000

    Update axiosConfig.jsx with new ip address

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.55.4:5001', // live  ← change 5000 to 5001
+  baseURL: 'http://3.27.199.78:5001', // live  ← change 5000 to 5001
   headers: { 'Content-Type': 'application/json' },
 });
 
